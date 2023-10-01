de311ce80cafbcfc0195cb3fb99931816c161d07
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-01 07:34:19 +0300

    `feat!:` implement basic client

--- client/main.py
+++ client/main.py
@@ -1,5 +1,31 @@
+import socket
+
+BUFSIZ = 1024
+
 def main():
-    pass
+    address = input('address: ')
+    port = int(input('port: '))
+
+    print('. . .')
+
+    sock = socket.socket()
+
+    sock.connect((address, port))
+
+    while True:
+        command = ' '.join(input('> ').split()) + '\n'
+
+        if command == '\n':
+            continue
+
+        sock.send(command.encode('ascii'))
+
+        if command == 'close\n' or command == 'exit\n' or command == 'quit\n':
+            break
+
+        print(sock.recv(BUFSIZ).decode('ascii'), end='')
+
+    sock.close()
 
 if __name__ == "__main__":
     main()
